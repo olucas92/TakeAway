a37a9f0b9b4892b78b1a045b0647027b06ae3e74
Oliver <o.lucas@live.com>
2015-01-04 20:35:10 +0000

    Some more tests written

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,10 @@ require 'menu'
 
 describe Menu do
   
+  it 'should be able to show the price of a pizza' do
+  end
+
+  it 'should be able to reject an order if not on the menu' do
+    endx
+
 end
